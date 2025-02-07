778665bef30c0898549b39b4e8d12b28c388742f
kittu64 <saikrishnar64@gmail.com>
2025-02-07 11:28:28 -0500

    initial commit

---
+++ src/components/About/Github.js
@@ -0,0 +1,12 @@
+import React from "react";
+import GitHubCalendar from "react-github-calendar";
+import { Row } from "react-bootstrap";
+
+function Github() {
+  return (
+    <Row >
+    </Row>
+  );
+}
+
+export default Github;
