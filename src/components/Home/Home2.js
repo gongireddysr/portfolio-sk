3c6a42c49574c2b0acca782d174f080aafdf499d
kittu64 <saikrishnar64@gmail.com>
2025-02-14 15:48:51 -0500

    commented the github

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -63,10 +63,10 @@ function Home2() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/sai-kittu"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
+                //href="https://github.com/sai-kittu"
+                //target="_blank"
+                //rel="noreferrer"
+                //className="icon-colour  home-social-icons"
                 >
                   <AiFillGithub />
                 </a>
